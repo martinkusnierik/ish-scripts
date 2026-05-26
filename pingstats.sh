b7f942ec81b6cd4df4c934926f8c0fe999baff7f
Martin Kušnierik <80431844+martinkusnierik@users.noreply.github.com>
2026-05-26 16:50:42 +0200

    Update pingstats.sh
    čistý výstup + štatistiky až po CTRL+C

--- pingstats.sh
+++ pingstats.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 . ./shared_stats.sh
-stats_enable_trap
 
 CONFIG_FILE="./ping.conf"
 [ -f "$CONFIG_FILE" ] && . "$CONFIG_FILE"
 : "${TARGET:=sme.sk}"
 
 LOGFILE="pinglog_$(date +"%Y-%m-%d_%H-%M-%S").txt"
+
+finish() {
+    echo ""
+    stats_print | tee -a "$LOGFILE"
+    exit 0
+}
+
+trap finish INT
 
 echo "Timestamped ping to $TARGET"
 echo "Logging to $LOGFILE"
@@ -17,10 +24,16 @@ echo ""
 while true; do
     LINE=$(ping -c 1 "$TARGET")
 
-    echo "$(date +"[%Y-%m-%d %H:%M:%S]") $LINE" | tee -a "$LOGFILE"
+    # vypíš ping na obrazovku
+    echo "$LINE"
 
+    # zapíš ping do logu s timestampom
+    echo "$(date +"[%Y-%m-%d %H:%M:%S]") $LINE" >> "$LOGFILE"
+
+    # extrahuj čas
     TIME=$(echo "$LINE" | grep -o "time=[0-9.]*" | cut -d= -f2)
     TIME_INT=${TIME%.*}
 
+    # pridaj do štatistík
     [ -n "$TIME_INT" ] && stats_add "$TIME_INT"
 done
