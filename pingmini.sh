30739b42ceb7ac4e040fdc72bd5af6e32660335e
Martin Kušnierik <80431844+martinkusnierik@users.noreply.github.com>
2026-05-27 14:25:31 +0200

    Update pingmini.sh
    pridanie časovej značky do logu ku každému ping

--- pingmini.sh
+++ pingmini.sh
@@ -8,8 +8,6 @@ CONFIG_FILE="./ping.conf"
 : "${TARGET:=sme.sk}"
 : "${MTU:=1500}"
 
-# Prepočet MTU → payload
-# MTU = payload + 28 (IP+ICMP header)
 PAYLOAD=$((MTU - 28))
 [ "$PAYLOAD" -lt 0 ] && PAYLOAD=0
 
@@ -35,20 +33,19 @@ while true; do
     RAW=$(ping -c 1 -s "$PAYLOAD" "$TARGET")
 
     LINE=$(echo "$RAW" | grep "bytes from")
+    TS=$(date +"[%Y-%m-%d %H:%M:%S]")
 
     if [ -n "$LINE" ]; then
-        # OK reply
         printf "!"
-        echo "$LINE" >> "$LOGFILE"
+        echo "$TS $LINE" >> "$LOGFILE"
 
         TIME=$(echo "$LINE" | grep -o "time=[0-9.]*" | cut -d= -f2)
         TIME_INT=${TIME%.*}
 
         stats_add "$TIME_INT"
     else
-        # timeout
         printf "."
-        echo "timeout seq=$SEQ mtu=$MTU" >> "$LOGFILE"
+        echo "$TS timeout seq=$SEQ mtu=$MTU" >> "$LOGFILE"
         stats_add "timeout"
     fi
 
